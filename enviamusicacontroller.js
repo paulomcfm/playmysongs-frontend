f9aa63bdba22dea22ae979df57584ac83b9ed044
Paulo Martin <paulomcfm@gmail.com>
2024-03-25 17:06:17 -0300

    Melhorando o controle de envio.

--- enviamusicacontroller.js
+++ enviamusicacontroller.js
@@ -1,3 +1,21 @@
+function validarCampos() {
+    var nome = document.getElementById('nome').value;
+    var artista = document.getElementById('artista').value;
+    var fileInput = document.getElementById('file');
+
+    if (nome === '' || artista === '') {
+        alert('Por favor, preencha todos os campos antes de fazer o upload.');
+        return false;
+    }
+
+    if (fileInput.files.length === 0) {
+        alert('Por favor, selecione um arquivo antes de fazer o upload.');
+        return false;
+    }
+
+    addMusica();
+}
+
 function addMusica(){
     const URL = "http://localhost:8080/apis/add-musica";
     var fdados = document.getElementById("fdados");
